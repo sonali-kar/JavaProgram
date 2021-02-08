572f2dafb15ef41eff65482460a5fcf3396513c5
sonali-kar <sonalikar590@gmail.com>
2021-02-08 21:39:03 +0530

    new

---
+++ MaxNumber.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class MaxNumber {
+
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+    if(a>b)
+    {
+        return a;
+    }
+    else
+    {
+     return b;
+    }
+}
+}
